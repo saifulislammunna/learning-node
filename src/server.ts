e24def4c06b129f6aeaf981e9e1655649ad8c172
saifulislammunna <saifulmunna28@gmail.com>
2026-05-11 19:27:39 +0600

    server-added

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,14 @@ const server : Server = createServer((req : IncomingMessage,res)=>{
 
     if(url === '/' && method === "GET"){
         // console.log("This  is Root route");
-        res.writeHead(200, {"content-type" : ""})
+        res.writeHead(200, {"content-type" : "application/json"})
+        res.end(JSON.stringify({message : "This is root route"}));
+    } else if(url?.startsWith('/products')){
+        res.writeHead(200, {"content-type" : "application/json"})
+        res.end(JSON.stringify({message : "This is products route"}));
+    }else{
+        res.writeHead(404, {"content-type" : "application/json"})
+        res.end(JSON.stringify({message : "Route not found!"}));
     }
 
 },
